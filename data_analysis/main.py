d1a9a947cc710fb80b18196855a9f49ed614bc10
Artem Vasenin <vasart169@gmail.com>
2016-12-17 06:10:45 +0000

    Remove classifier saving

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -9,6 +9,8 @@ import pickle
 # temp fix, not fixing since not gonna use this in the future
 import warnings
 
+USE_SAVED_DATA = False
+
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 MATCH_LIST = json.loads(open("../match_stats.json", 'r').read())["matches500"]
@@ -16,7 +18,7 @@ MATCH_LIST = json.loads(open("../match_stats.json", 'r').read())["matches500"]
 classifier_name = 'classifier.pkl'
 dataset_name = 'dataset.pkl'
 
-if os.path.isfile(dataset_name):
+if os.path.isfile(dataset_name) and USE_SAVED_DATA:
     dataset = pickle.load(open(dataset_name, 'rb'))
 else:
     X = []
@@ -30,21 +32,15 @@ else:
         X.append(results["features"])
         y.append(results["result"])
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
-    dataset = {"X_train": X_train, "X_test": X_test, "y_train": y_train, "y_test": y_test}
+    dataset = {'X': X, 'y': y}
 
     with open(dataset_name, 'wb') as output:
         pickle.dump(dataset, output, pickle.HIGHEST_PROTOCOL)
 
-if not os.path.isfile(classifier_name):
-    clf = pickle.load(open(classifier_name, 'rb'))
-else:
-    clf = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(100, 100))
-    clf.fit(dataset["X_train"], dataset["y_train"])
-
-    with open(classifier_name, 'wb') as output:
-        pickle.dump(clf, output, pickle.HIGHEST_PROTOCOL)
+X_train, X_test, y_train, y_test = train_test_split(dataset['X'], dataset['y'], test_size=0.1)
+clf = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(100, 100))
+clf.fit(X_train, y_train)
 
 MATCH_LIST = json.loads(open("../match_stats.json", 'r').read())["matches500"]
 
-print(clf.score(dataset["X_test"], dataset["y_test"]))
+print(clf.score(X_test, y_test)
