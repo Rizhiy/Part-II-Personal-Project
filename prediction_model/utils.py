23e1ed4d0170a0e09da6b55a72dd4f2818c4def9
Artem Vasenin <vasart169@gmail.com>
2017-04-19 17:12:56 +0100

    Move all required functions into the same package, fixed model so it compiles

--- prediction_model/utils.py
+++ prediction_model/utils.py
@@ -1,11 +1,26 @@
+from enum import Enum
+
 import keras
 import numpy as np
 import tensorflow as tf
 from keras.layers import Dense, Dropout
 
-from data_analysis.Match import get_match_stats
-from data_analysis.Player import Stats
-from prediction_model import PLAYERS_PER_TEAM, PLAYERS, PLAYER_DIM
+from prediction_model import PLAYERS_PER_TEAM, PLAYERS, PLAYER_DIM, MATCHES
+
+
+class Stats(Enum):
+    KILLS = "kills"
+    DEATHS = "deaths"
+    ASSISTS = "assists"
+    LEVEL = "level"
+    GPM = "gold_per_min"
+    XPM = "xp_per_min"
+    CREEPS = "last_hits"
+    DENIES = "denies"
+    # Those three values are missing from first half of the dataset,
+    # TOWER_DMG = "tower_damage"
+    # HERO_DMG = "hero_damage"
+    # HEALING = "hero_healing"
 
 
 def min_log(tensor):
@@ -14,7 +29,8 @@ def min_log(tensor):
 
 def log_normal(x, mu, sigma):
     error = -(tf.pow((x - mu) / sigma, 2) / 2 + min_log(sigma) + min_log(2 * np.pi) / 2)
-    return tf.reduce_sum(error, 1)
+    error = tf.squeeze(error)
+    return tf.reduce_sum(error, 0)
 
 
 def make_sql_nn(in_dim: int, out_dim: int, dropout: bool = False, first_activation='relu'):
@@ -32,36 +48,71 @@ def make_mu_and_sigma(nn, tensor):
     return mu, sigma
 
 
+def get_match_data(match_id):
+    return MATCHES[match_id]
+
+
+def get_player_side(player_slot):
+    """
+    :param player_slot:
+    :type player_slot: int
+    :return: Returns true if radiant
+    :rtype: bool
+    """
+    mask = 0b10000000
+    return mask & player_slot == 0
+
+
+def get_player_data(match_id: int) -> dict:
+    """
+    :param match_id: id of the required match
+    :type match_id: int
+    :return: dict with four values: "radiant_players", "dire_players", "match_id" and "match_data"
+    :rtype: dict
+    """
+    match_data = get_match_data(match_id)
+    radiant_players = []
+    dire_players = []
+    for player in match_data["players"]:
+        if get_player_side(player["player_slot"]):
+            radiant_players.append(player)
+        else:
+            dire_players.append(player)
+    return {
+        "radiant_players": radiant_players,
+        "dire_players": dire_players,
+        "match_id": match_id,
+        "match_data": match_data
+    }
+
+
 def get_match_arrays(match_id):
-    match_stats = get_match_stats(match_id)
+    match_stats = get_player_data(match_id)
     radiant_ids = []
     dire_ids = []
     player_results = []
     players_stats = match_stats["radiant_players"] + match_stats["dire_players"]
     for idx, player_stat in enumerate(players_stats):
-        player_results.append(player_stat[Stats.KILLS])
-        player_results.append(player_stat[Stats.DEATHS])
-        player_results.append(player_stat[Stats.ASSISTS])
-        player_results.append(player_stat[Stats.GPM])
-        player_results.append(player_stat[Stats.XPM])
-        player_results.append(player_stat[Stats.LEVEL])
-        player_results.append(player_stat[Stats.CREEPS])
-        player_results.append(player_stat[Stats.DENIES])
+        player_result = [player_stat[Stats.KILLS.value], player_stat[Stats.DEATHS.value],
+                         player_stat[Stats.ASSISTS.value], player_stat[Stats.LEVEL.value],
+                         player_stat[Stats.GPM.value], player_stat[Stats.XPM.value],
+                         player_stat[Stats.CREEPS.value], player_stat[Stats.DENIES.value]]
+        player_results.append(player_result)
         if idx < PLAYERS_PER_TEAM:
-            radiant_ids = player_stat["account_id"]
+            radiant_ids.append(player_stat["account_id"])
         else:
-            dire_ids = player_stat["account_id"]
+            dire_ids.append(player_stat["account_id"])
     player_skills = []
     for player_id in radiant_ids + dire_ids:
         if player_id not in PLAYERS:
-            PLAYERS[player_id] = [0] * PLAYER_DIM
-        player_skills += PLAYERS[player_id]
+            PLAYERS[player_id] = [0] * PLAYER_DIM + [1] * PLAYER_DIM
+        player_skills.append(PLAYERS[player_id])
     if match_stats["match_data"]["radiant_win"]:
-        team_results = [1,0]
+        team_results = [1, 0]
     else:
-        team_results = [0,1]
+        team_results = [0, 1]
     return {
-        "player_skills": player_skills,
-        "player_results": player_results,
-        "team_results": team_results
+        "player_skills": [player_skills],
+        "player_results": [player_results],
+        "team_results": [team_results]
     }
