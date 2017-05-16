b9be0a9fa44738186bcc171c38f322d024bbf8c1
Artem Vasenin <vasart169@gmail.com>
2017-05-16 09:44:59 +0100

    More graphs

--- data_analysis/Graphs.py
+++ data_analysis/Graphs.py
@@ -9,16 +9,13 @@ import data_analysis
 from data_analysis import Learning
 
 
-def density_hist(data: list, label="", figure=True, title=""):
-    if figure:
-        plt.figure()
-        plt.title(title)
+def density_hist(data: list, label=""):
     bins = max(data) - min(data)
     if bins < 10:
         bins = 50
     data = np.array(data)
     data = data[~np.isnan(data)]  # Remove NaN
-    if bins < 50:
+    if bins < 20:
         plt.hist(data, bins=bins, normed=True)
     else:
         try:
@@ -58,7 +55,6 @@ def fit_gamma_and_gaussian(stat: Enum):
     data = data[~np.isnan(data)]
     mean = np.mean(data)
     variance = np.var(data)
-    std = np.sqrt(variance)
     k = np.square(mean) / variance
     theta = variance / mean
     max_value = np.percentile(data, 99)
@@ -75,7 +71,7 @@ def fit_gamma_and_gaussian(stat: Enum):
              format(k, theta, int(chisquare(hist[0], y1[:-1])[0])))
     param = norm.fit(data)
     y2 = norm.pdf(x, *param[:-2], loc=param[-2], scale=param[-1]) * len(data) * max_value / bins
-    plt.plot(x, y2, label=r"Fitted gaussian:\\ $\mu = {}$, $\sigma = {}$\\ $\chi^2 = {}$".
+    plt.plot(x, y2, '--', label=r"Fitted gaussian:\\ $\mu = {}$, $\sigma = {}$\\ $\chi^2 = {}$".
              format(int(param[-2]), int(param[-1]), int(chisquare(hist[0], y2[:-1])[0])))
     plt.gca().axes.get_yaxis().set_ticks([])
     plt.legend()
@@ -87,11 +83,14 @@ def raw_stat_hist(stat: Enum):
     for match in data_analysis.MATCH_LIST:
         for player in data_analysis.MATCHES[match]["players"]:
             data.append(player[stat.value])
+    max = np.percentile(data, 99)
+    data = np.array(data)
+    data = data[data < max]
     density_hist(data, "Distribution of values")
-    value = np.percentile(data, 99, 0)
-    plt.axvline(x=value, label="99% percentile", color='r')
-    value = np.percentile(data, 1, 0)
-    plt.axvline(x=value, label="1% percentile", color='g')
+    # value = np.percentile(data, 99, 0)
+    # plt.axvline(x=value, label="99% percentile", color='r', linestyle='--')
+    # value = np.percentile(data, 1, 0)
+    # plt.axvline(x=value, label="1% percentile", color='g', linestyle='-.')
     plt.legend()
 
 
@@ -103,7 +102,6 @@ def error_stat_hist(stat: Enum, regr, dataset):
     xs = np.linspace(min(results), max(results), 128)
     density.covariance_factor = lambda: .25
     density._compute_covariance()
-    plt.figure()
     plt.title(stat.value + " error")
     plt.plot(xs, density(xs))
     plt.draw()
@@ -128,6 +126,8 @@ def raw_trueskill_winrate(dataset):
     radiant_sums = []
     dire_sums = []
     for idx, match_id in enumerate(dataset["match_ids"]):
+        if match_id not in data_analysis.MATCHES:
+            continue
         radiant_sum = []
         dire_sum = []
         for idx2, feature in enumerate(dataset["features"][idx]):
@@ -140,24 +140,18 @@ def raw_trueskill_winrate(dataset):
         outcomes.append(data_analysis.MATCHES[match_id]["radiant_win"])
         radiant_sums.append(np.sum(radiant_sum))
         dire_sums.append(np.sum(dire_sum))
-    plt.figure()
-    plt.title("winrate")
-    fig, ax = plt.subplots(figsize=(12, 12))
+    fig, ax = plt.subplots(figsize=(7.5, 7.5))
     ax.set_title("winrate")
     for idx, outcome in enumerate(outcomes):
         if outcome:
             ax.plot(radiant_sums[idx], dire_sums[idx], marker='s', color="red")
         else:
             ax.plot(radiant_sums[idx], dire_sums[idx], marker='o', color="green")
-    plt.xlabel("Radiant TrueSkill sum")
-    plt.ylabel("Dire TrueSkill sum")
-    # PdfPages('winrate.pdf').savefig()
     plt.draw()
 
 
 def plot_estimation(mean, std, title, value):
     x = np.linspace(mean - std * 4, mean + std * 4, 100)
-    plt.figure()
     plt.title(title)
     plt.plot(x, mlab.normpdf(x, mean, std),
              label="Predicted probability:\n mu={}, sigma={}".format(int(mean), int(std)))
--- data_analysis/main.py
+++ data_analysis/main.py
@@ -3,11 +3,14 @@ import pickle
 
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.ticker import ScalarFormatter, FuncFormatter
-from sklearn.linear_model import Ridge
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.linear_model import Ridge, LinearRegression
+from sklearn.neural_network import MLPRegressor
 
 import data_analysis.Learning
 from data_analysis import Learning
+from data_analysis.Graphs import prediction_hist
+from data_analysis.Player import Stats
 
 USE_SAVED_DATA = True
 dataset_name = 'dataset.pkl'
@@ -16,8 +19,9 @@ if not (os.path.isfile(dataset_name) and USE_SAVED_DATA):
     Learning.generate_features(data_analysis.MATCH_LIST, dataset_name)
 dataset = pickle.load(open(dataset_name, 'rb'))
 
-regr = Ridge(alpha=0.1, normalize=True)
-# regr = MLPRegressor(hidden_layer_sizes=(50,))
+# regr = GaussianProcessRegressor()
+# regr = Ridge(alpha=0.1, normalize=True)
+regr = MLPRegressor(hidden_layer_sizes=(100,))
 
 # player_slot = 7
 # stat = Stats.XPM
@@ -34,13 +38,13 @@ dataset = Learning.new_format_to_old(dataset)
 # targets = Learning.generate_targets(dataset['match_ids'], 0, Stats.GPM)
 # print(np.mean(cross_val_score(regr, dataset['features'],targets,cv=10)))
 
-# for stat in Stats:
-#     results = Learning.test_stat(stat, regr, dataset)
-#     print()
-#     print("Stat: {}".format(stat.value))
-#     print("{:>7} = {}\n{:>7} = {}\n{:>7} = {}".format("Mean", np.mean(results),
-#                                                       "Highest", max(results),
-#                                                       "Lowest", min(results)))
+for stat in Stats:
+    results = Learning.test_stat(stat, regr, dataset)
+    print()
+    print("Stat: {}".format(stat.value))
+    print("{:>7} = {}\n{:>7} = {}\n{:>7} = {}".format("Mean", np.mean(results),
+                                                      "Highest", max(results),
+                                                      "Lowest", min(results)))
 
 # duration_results = Learning.test_duration(regr, dataset)
 # print()
@@ -65,52 +69,75 @@ dataset = Learning.new_format_to_old(dataset)
 # Graphs.prediction_hist(Stats.ASSISTS, regr, dataset)
 
 # Graphs.fit_gaussian(Stats.GPM)
-# Graphs.fit_gamma_and_gaussian(Stats.GPM)
 # Graphs.raw_trueskill_winrate(dataset)
 # print("Graphs drawn")
 # Graphs.raw_stat_hist(Stats.XPM)
-# plt.plot([1.10, .869, .823, .808, .802, .801, .803, .819, .833, .838, .851, .852, .862], label="Validation loss")
-# plt.plot([1.05, .858, .817, .775, .750, .725, .708, .700, .692, .683, .675, .675, .667], label="Training loss")
+fig = plt.figure(figsize=(12, 7.5))
+# plt.plot([1.10, .869, .823, .808, .802, .801, .803, .819, .833, .838, .851, .852, .862], label=r"Validation loss")
+# plt.plot([1.05, .858, .817, .775, .750, .725, .708, .700, .692, .683, .675, .675, .667], label=r"Training loss")
 # plt.axis([0, 13, .65, 1.15])
 # plt.ylabel('Loss')
 # plt.xlabel('Co-ordinate descent iterations')
 # plt.tick_params(axis='y', which='both', left='off', right='off', labelleft='off')
 # plt.legend()
 
-H = np.array([[.960, .835, .784, .777, .772, .769],
-              [.896, .866, .772, .781, .807, .785],
-              [.990, .949, .799, .791, .806, .799],
-              [.971, .965, .815, .798, .798, .813],
-              [1.01, .984, 1.02, .802, .809, .789],
-              [1.02, 1.01, 1.01, 1.01, .812, .820],
-              [1.03, 1.00, 1.02, .911, .926, .797],
-              ])  # added some commas and array creation code
+# H = np.array([[.1897, .2184, .2348, .2449, .2543],
+#               [.1869, .2113, .2327, .2431, .2569],
+#               [.1836, .2117, .2332, .2456, .2567],
+#               [.1803, .2132, .2317, .2440, .2556],
+#               [.1873, .2151, .2366, .2499, .2580],
+#               [.1876, .2075, .2349, .2439, .2531],
+#               ])  # added some commas and array creation code
+#
+# H = np.swapaxes(H, 0, 1)
+# H = H[::-1]
+#
+# ax = fig.add_subplot(111)
+# ax.set_title('Hyper-parameter search results')
+# plt.imshow(H, cmap=plt.get_cmap('plasma'), interpolation='nearest', extent=[0.707, 45.25, 0.707, 22.63])
+# ax.set_aspect('equal')
+# plt.xscale('log', basex=2)
+# plt.yscale('log', basey=2)
+# plt.xlabel("Size of team skills")
+# plt.ylabel("Size of player skills")
+# for axis in [ax.xaxis, ax.yaxis]:
+#     axis.set_major_formatter(ScalarFormatter())
+#
+#
+# def fmt(x, pos):
+#     return '' if x == 0 else '{:.0f}'.format(x)
+#
+#
+# ax.xaxis.set_major_formatter(FuncFormatter(fmt))
+# ax.yaxis.set_major_formatter(FuncFormatter(fmt))
+#
+# plt.colorbar(orientation='vertical', label=r"$R^2$")
 
-# fig = plt.figure(figsize=(6, 3.2))
-fig = plt.figure()
+# high = [0.811, 0.805, 0.796, 0.793, 0.795, 0.796, 0.793, 0.792, 0.796, 0.787, 0.792, 0.802, 0.799, 0.801, 0.797, 0.798,
+#         0.791, 0.81, 0.801, 0.798, 0.802]
+# medium = [1.002, 1.000, 0.997, 0.882, 0.825, 0.817, 0.807, 0.803, 0.8, 0.799, 0.795, 0.797, 0.798, 0.795, 0.794, 0.796,
+#           0.796, 0.795, 0.797, 0.796, 0.797]
+# low =
+# fit_gamma_and_gaussian(Stats.GPM)
 
-ax = fig.add_subplot(111)
-ax.set_title('Hyper-parameter search results')
-plt.imshow(H, cmap=plt.get_cmap('plasma_r'), interpolation='nearest', extent=[0.707, 45.25, 90.51, 0.707])
-ax.set_aspect('equal')
-plt.xscale('log', basex=2)
-plt.yscale('log', basey=2)
-plt.ylabel("Size of team skills")
-plt.xlabel("Size of player skills")
-for axis in [ax.xaxis, ax.yaxis]:
-    axis.set_major_formatter(ScalarFormatter())
+# raw_stat_hist(Stats.GPM)
+# plt.tick_params(
+#     axis='y',          # changes apply to the x-axis
+#     which='both',      # both major and minor ticks are affected
+#     left='off',      # ticks along the bottom edge are off
+#     right='off',         # ticks along the top edge are off
+#     labelleft='off')
+# plt.ylabel("Frequency")
+# plt.xlabel("Variable value")
 
+# plt.plot([0, 0, .181, .227, .265, .256, .229, .218, ])
+# plt.xlabel("Number of months used")
+# plt.ylim([0.15,0.3])
+# plt.ylabel(r"Average $R^2$")
+# plt.xlim([2, 7])
 
-def fmt(x, pos):
-    return '' if x == 0 else '{:.0f}'.format(x)
-
-
-ax.xaxis.set_major_formatter(FuncFormatter(fmt))
-ax.yaxis.set_major_formatter(FuncFormatter(fmt))
-
-plt.colorbar(orientation='vertical').ax.invert_yaxis()
-plt.show()
-
+# raw_trueskill_winrate(dataset)
+# prediction_hist(Stats.ASSISTS, regr, dataset)
 plt.show()
 
 # selected_features = Learning.choose_features(Stats.GPM, regr, dataset)
