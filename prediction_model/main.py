be5e066bf24c76c6b6ebb2d29365dc47651f9424
Artem Vasenin <vasart169@gmail.com>
2017-05-22 04:48:07 +0100

    Improve debug output

--- prediction_model/main.py
+++ prediction_model/main.py
@@ -7,9 +7,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from keras.layers.core import K
+from scipy.interpolate import spline
 
 from prediction_model import SESSION, MATCH_LIST, BATCH_SIZE, PLAYERS_PER_TEAM, NUM_OF_TEAMS, RETRAIN, PLAYER_GAMES, \
-    BOSTON_MAJOR_LAST_GAME_ID, TI_6_LAST_GAME_ID, DEBUG, PLAYER_PERFORMANCES, PLAYER_SKILLS
+    BOSTON_MAJOR_LAST_GAME_ID, TI_6_LAST_GAME_ID, DEBUG, PLAYER_PERFORMANCES, PLAYER_SKILLS, PLAYER_DIM, FEAR_ID
+from prediction_model.Graphs import density_hist
 from prediction_model.match_processing_model import player_results_split, player_to_results_param0, \
     player_to_results_param1, \
     loss as inference_loss, player_performance as match_performances, player_skills, player_results, \
@@ -18,7 +20,7 @@ from prediction_model.skill_update_model import loss as update_loss, player_pre_
     player_next_performance, post_skill, player_post_skill
 from prediction_model.utils import create_data_set, get_new_batch, store_player_performances, get_skill_batch, \
     update_player_skills, create_player_games, get_test_batch, split_list, make_mu_and_sigma, make_k_and_theta, \
-    create_validation_sets, reset_stats
+    create_validation_sets, reset_stats, get_skill
 
 test_result = player_results_split[0]
 test2_result = player_to_results_param0[0] * player_to_results_param1[0]
@@ -36,17 +38,17 @@ if TI_6_LAST_GAME_ID in t_list:
     t_list.remove(TI_6_LAST_GAME_ID)
     validation_list.append(TI_6_LAST_GAME_ID)
 cross_validation_set = create_validation_sets(validation_list)
-
-
+num_t_batches = int(len(t_list) / BATCH_SIZE)
 
 file_name = "predictions.pkl"
 
-
 if RETRAIN:
     for idx, test_list in enumerate(cross_validation_set):
+        # Cross validation
         SESSION.run(init)  # Reset model
         train_set = deepcopy(cross_validation_set)
-        del train_set[idx]
+        test_list = train_set[0]
+        del train_set[0]
         train_list = [item for sublist in train_set for item in sublist]
         num_train_batches = int(len(train_list) / BATCH_SIZE)
         reset_stats()  # Reset player skills
@@ -66,6 +68,7 @@ if RETRAIN:
             if timer > 100:
                 break
             if stopping_counter > 4:
+                # Evaluate system
                 K.set_learning_phase(False)
                 num_test_batches = int(len(test_list) / BATCH_SIZE)
                 predicted = []
@@ -129,6 +132,7 @@ if RETRAIN:
                 stopping_counter = 0
                 K.set_learning_phase(True)
             if phase > 0:
+                # Train prediction model
                 batch = get_new_batch(counter, train_list, num_train_batches)
                 _, loss_step, player_performances, performance_estimate, test, test2 = SESSION.run(
                     (inference_train_step, inference_loss, match_performances, player_performance_estimate, test_result,
@@ -139,7 +143,8 @@ if RETRAIN:
                 result = (result * alpha + loss_step) / (1 + alpha)
                 store_player_performances(batch["match_ids"], np.swapaxes(player_performances, 0, 1))
                 if batch["switch"]:
-                    print(timer)
+                    if DEBUG > 1:
+                        print("Phase: {}".format(phase))
                     random.shuffle(train_list)
                     phase -= 1
                     pass_num += 1
@@ -153,6 +158,7 @@ if RETRAIN:
                     result = 0
                 counter += 1
             else:
+                # Train skill update model
                 player_loss = []
                 ids = list(PLAYER_GAMES.keys())
                 random.shuffle(ids)
@@ -178,44 +184,59 @@ if RETRAIN:
             if np.math.isnan(loss_step):
                 print("Nan loss", file=sys.stderr)
                 break
+    K.set_learning_phase(False)
+    predicted = []
+    result = []
+    error = []
+    for seed in range(num_t_batches):
+        predicted_player_skills = np.array(batch["player_skills"])
+        predicted_result = SESSION.run(predicted_player_result)
+        predicted_result = np.swapaxes(predicted_result, 0, 1)
+        for i in range(len(predicted_result)):
+            for player in range(len(predicted_result[i])):
+                predicted.append(predicted_result[i][player])
+                result.append(batch["player_results"][i][player])
+                error.append(batch["player_results"][i][player] - predicted_result[i][player])
+    data = {"predicted": np.array(predicted), "result": np.array(result), "error": np.array(error)}
+    pickle.dump(data, open(file_name, "wb"))
 else:
     data = pickle.load(open(file_name, "rb"))
-K.set_learning_phase(False)
 
-if DEBUG:
-    print(PLAYER_SKILLS[TI_6_LAST_GAME_ID])
-    print(PLAYER_PERFORMANCES[TI_6_LAST_GAME_ID])
+
 plt.figure(figsize=(12, 7.5))
-# predicted = np.swapaxes(data["predicted"], 0, 1)
-# error = np.swapaxes(data["error"], 0, 1)
-# result = np.swapaxes(data["result"], 0, 1)
-# stat = 1
-# density_hist(predicted[stat])
-# density_hist(result[stat])
-# # plt.legend()
-# plt.figure()
-# density_hist(error[stat])
-# plt.show()
-
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 plt.rc('font', size=16)
+if DEBUG > 1 :
+    print(PLAYER_SKILLS[TI_6_LAST_GAME_ID])
+    print(PLAYER_PERFORMANCES[TI_6_LAST_GAME_ID])
+    predicted = np.swapaxes(data["predicted"], 0, 1)
+    error = np.swapaxes(data["error"], 0, 1)
+    result = np.swapaxes(data["result"], 0, 1)
+    stat = 1
+    density_hist(predicted[stat])
+    density_hist(result[stat])
+    # plt.legend()
+    plt.figure()
+    density_hist(error[stat])
+    plt.show()
 
-# skills = []
-# for match_id in PLAYER_GAMES[FEAR_ID]["all"]:
-#     skills.append(get_skill(FEAR_ID, match_id, True))
-# skills = np.array(skills)
-#
-# l = len(skills[:, 0])
-# xnew = np.linspace(0, l, 128)
-#
-# for i in range(PLAYER_DIM):
-#     plt.plot(xnew, spline(range(l), skills[:, i], xnew))
-# plt.tick_params(axis='both', which='both', bottom='off', top='off', labelbottom='off', right='off', left='off',
-#                 labelleft='off')
-# plt.xlabel("Time")
-# plt.ylabel("Skill level")
-# plt.show()
+if DEBUG > 2:
+    skills = []
+    for match_id in PLAYER_GAMES[FEAR_ID]["all"]:
+        skills.append(get_skill(FEAR_ID, match_id, True))
+    skills = np.array(skills)
+
+    l = len(skills[:, 0])
+    xnew = np.linspace(0, l, 128)
+
+    for i in range(PLAYER_DIM):
+        plt.plot(xnew, spline(range(l), skills[:, i], xnew))
+    plt.tick_params(axis='both', which='both', bottom='off', top='off', labelbottom='off', right='off', left='off',
+                    labelleft='off')
+    plt.xlabel("Time")
+    plt.ylabel("Skill level")
+plt.show()
 
 error = data["error"]
 result = data["result"]
